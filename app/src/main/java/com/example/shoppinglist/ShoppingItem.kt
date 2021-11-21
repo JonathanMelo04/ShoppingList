63d71b8760e5428caee36f1829fde3b6e5efac9b
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-21 16:16:58 -0300

    created variables and columns, representing name and quantity

--- app/src/main/java/com/example/shoppinglist/ShoppingItem.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingItem.kt
@@ -1,8 +1,14 @@
 package com.example.shoppinglist
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 
 @Entity(tableName = "shopping_items")
-data class ShoppingItem {
+data class ShoppingItem(
+    @ColumnInfo(name = "item_name")
+    var name: String,
+    @ColumnInfo(name = "item_amount")
+    var amount: Int,
+) {
 
 }
