8aa4e753ee334c836e6e8ac3ab24379243daaf15
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-22 00:23:23 -0300

    Added PrimaryKey

--- app/src/main/java/com/example/shoppinglist/ShoppingItem.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingItem.kt
@@ -2,6 +2,7 @@ package com.example.shoppinglist
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "shopping_items")
 data class ShoppingItem(
@@ -10,5 +11,6 @@ data class ShoppingItem(
     @ColumnInfo(name = "item_amount")
     var amount: Int,
 ) {
-
+@PrimaryKey(autoGenerate = true)
+var id: Int? = null
 }
