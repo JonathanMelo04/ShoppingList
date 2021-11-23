73531df908b7109f44a59e3b2d3b13a1c7ff1a6d
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-22 23:36:29 -0300

    Added delete function

--- app/src/main/java/com/example/shoppinglist/ShoppingDao.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingDao.kt
@@ -1,6 +1,7 @@
 package com.example.shoppinglist
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 
@@ -8,6 +9,9 @@ import androidx.room.OnConflictStrategy
 interface ShoppingDao {
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
-fun upsert(item: ShoppingItem)
+  suspend fun upsert(item: ShoppingItem)
+
+  @Delete
+  suspend fun delete(item: ShoppingItem)
 
 }
