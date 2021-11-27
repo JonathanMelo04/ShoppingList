82aa0298e2c4da9a5386bff0a64481521ec88344
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-27 11:51:10 -0300

    Added functions to ShoppingViewModel

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoppinglist
+package com.example.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppinglist.R
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
